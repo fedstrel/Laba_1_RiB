62e3a8582d84cb99970766d878956c3847f52401
Fedor Strelnikov <73533082+fedstrel@users.noreply.github.com>
2022-04-28 20:14:25 +0300

    Add files via upload

---
+++ laba_1_RiB.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Task 13. Put into a .txt file absolute path from which the .sh command is being executed.
+
+output="output.txt";
+# check if there are any arguments
+if [ $# != 0 ];
+then
+	output=$1;
+	output+=".txt";
+fi
+#pwd is a command that shows the absolute path to the file
+#> reroutes output of the command into a file
+pwd > $output;
